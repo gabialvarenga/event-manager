6d60a3d574fa05785c51f8e76e2cc65effb0c3b7
gabi <gabialvarenga4@gmail.com>
2025-09-06 19:25:58 -0300

    refactor: restructuring the entire front end to improve organization

--- src/back/event-manager/src/main/java/com/dtidigital/event_manager/exception/EventNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dtidigital.event_manager.exception;
-
-public class EventNotFoundException extends RuntimeException {
-    public EventNotFoundException(String message) {
-        super(message);
-    }
-}
