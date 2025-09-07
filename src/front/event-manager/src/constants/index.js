d98aa5075743d666cac450259138fddd3edcfaa3
gabi <gabialvarenga4@gmail.com>
2025-09-07 17:53:29 -0300

    fix: Update API_BASE_URL to use production endpoint

--- src/front/event-manager/src/constants/index.js
+++ src/front/event-manager/src/constants/index.js
@@ -1,6 +1,6 @@
 // Configurações da API
 export const API_CONFIG = {
-  BASE_URL: 'http://localhost:8080/api',
+  BASE_URL: 'https://event-manager-grkf.onrender.com/api',
   TIMEOUT: 10000,
   HEADERS: {
     'Content-Type': 'application/json',
